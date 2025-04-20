6406f203fabb56d6df054ae8ba22f604c9ea16d0
hadyanbakti <123660163+hadyanbakti@users.noreply.github.com>
2025-04-20 19:08:23 +0700

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,13 @@ import express from "express";
 import cors from "cors";
 import UserRoute from "./routes/UserRoute.js";
 
-const port = process.env.PORT || 3000;
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
-});
+
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 app.use(UserRoute);
-
-app.listen(3000, ()=> console.log('Server up and running...'));
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
